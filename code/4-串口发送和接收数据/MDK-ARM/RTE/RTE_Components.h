e580f2a22126724f340c03378c15d0e24e848492
LAPTOP-Q2CI4ABQ\seven <1784738842@qq.com>
2021-01-14 23:40:08 +0800

    串口发送和接收数据

---
+++ code/4-串口发送和接收数据/MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'STM32L431RC_BearPiUART' 
+ * Target:  'STM32L431RC_BearPiUART' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
